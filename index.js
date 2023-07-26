efee34ac89dccfabbcb2ca40c4819a8a5db3c47d
Timuçin Alpay Bohur <bohuralpay@gmail.com>
2023-07-26 20:29:05 +0300

    ödev teslimi

--- index.js
+++ index.js
@@ -39,11 +39,10 @@ Aşağıdakileri yapmak için aşağıdaki kopyalama işlevini kullanın:
    2. Alınan dizinin bir kopyasını döndür
 */
 
-
-function kopyala(/*kod buraya*/){
-  /*kod buraya*/
+function kopyala(tat) {
+  const sonuc = tat.slice();
+  return sonuc;
 }
-
 
 /* Görev 2:
 Bir dizinin tam olarak 25 çeşit olduğunu onaylayın. İşleviniz şunları kabul etmelidir:
@@ -55,11 +54,13 @@ Bir dizinin tam olarak 25 çeşit olduğunu onaylayın. İşleviniz şunları ka
 Örneğin: dizi25Cesit(orijinalTatlar) kodunuz düzgün çalışıyorsa true değerini döndürür.
 */
 
-
-function dizi25Cesitmi(/*kod buraya*/){
-  /*kod buraya*/
+function dizi25Cesitmi(tat) {
+  if (tat.length == 25) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
 
 /* Görev 3:
 Pastane sahibi size yeni bir lezzet fikriyle geldi: Kakule! Bunun da çok tutacağından çok emin. Bu lezzeti eklemek için diziyi değiştirmeniz gerekir.
@@ -73,11 +74,11 @@ Aşağıdakileri yapmak için cesitEkle işlevini kullanın:
   Örneğin: cesitEkle(orijinalTatlar, "Kakule") işlevi doğru çalıştığında ["Kakule", "Muz",..."Vanilya"] şeklinde dönmelidir
 */
 
-
-function cesitEkle(/*kod buraya*/){
-  /*kod buraya*/
+function cesitEkle(tat, yenitat) {
+  tat.unshift(yenitat);
+  return tat;
 }
-
+console.log(cesitEkle(orijinalTatlar, "Kakule"));
 
 /* Cörev 4:
 
@@ -91,11 +92,11 @@ Aşağıdakileri yapmak için sonCesitiKaldir işlevini kullanın:
    Örneğin: sonCesitiKaldir(orijinalTatlar) çalıştırıldığında ["Kakule", "Muz",..."Çilek"] döndürülür.
 */
 
-
-function sonCesitiKaldir(/*kod buraya*/){
-  /*kod buraya*/
+function sonCesitiKaldir(tat) {
+  tat.pop();
+  return tat;
 }
-
+console.log(sonCesitiKaldir(orijinalTatlar));
 
 /* Görev 5:
 Dizideki belirli bir indeksteki çeşniyi döndüren bir işlev yazın.
@@ -108,11 +109,11 @@ Aşağıdakileri yapmak için aşağıdaki indekstekiCesitiGetir işlevini kulla
    Örneğin: indekstekiCesitiGetir(orijinalTatlar, 2) çalıştırılmasıyla, Kakule'in başarıyla eklendiği varsayarsak sonuç "Ceviz" olucaktır.
 */
 
-function indekstekiCesitiGetir(/*kod buraya*/){
-  /*kod buraya*/
+function indekstekiCesitiGetir(tat, gelen) {
+  let sonuc = tat[gelen];
+  return sonuc;
 }
-
-
+console.log(indekstekiCesitiGetir(orijinalTatlar, 2));
 /* Görev 6:
 
 Firma, ürün yelpazesine daha fazla lezzet eklemek istediğinden, sadece ilk veya son aromayı keyfi olarak çıkarmak yerine, aroma adına göre aromaları kaldırmaları gerektiğini fark ederler. Göreviniz, verilen bir dizinde, ada göre bu tadı diziden çıkarmak.
@@ -128,10 +129,15 @@ Aşağıdakileri yapmak için ismeGoreCesitCikar işlevini kullanın:
   İPUCU: Bunun için .splice() kullanabilirsiniz.
 */
 
-function ismeGoreCesitCikar(/*kod buraya*/){
-  /*kod buraya*/
+function ismeGoreCesitCikar(tat, lezzet) {
+  for (let i = 0; i < tat.length; i++) {
+    if (tat[i] == lezzet) {
+      tat.splice(i, 1);
+    }
+  }
+  return tat;
 }
-
+console.log(ismeGoreCesitCikar(orijinalTatlar, "Tarçın"));
 
 /* Görev 7:
 
@@ -153,12 +159,15 @@ Aşağıdakileri yapmak için ismeGoreFiltrele işlevini kullanın:
   Bu sorunu çözmek için GELİŞMİŞ DİZİ YÖNTEMLERİNİ (yani .filter) KULLANMAYIN.
 */
 
-
-function ismeGoreFiltrele(/*kod buraya*/){
-  /*kod buraya*/
+function ismeGoreFiltrele(tatlar, lezzet) {
+  let sonuc = [];
+  for (let i = 0; i < tatlar.length; i++) {
+    if (tatlar[i].includes(lezzet)) {
+      sonuc.push(tatlar[i]);
+    }
+  }
+  return sonuc;
 }
-
-
 
 /* ALIŞTIRMA */
 
@@ -172,10 +181,21 @@ Aşağıdakileri yapmak için ortalamaKelimeSayisi işlevini kullanın:
    Örneğin: ortalamaKelimeSayisi(orijinalTatlar) 0 ile 2 arasında bir sayı döndürmelidir.
 */
 
-function ortalamaKelimeSayisi(/*kod buraya*/){
-  /*kod buraya*/
+function ortalamaKelimeSayisi(dizi) {
+  let toplamKelimeSayisi = 0;
+  for (let i = 0; i < dizi.length; i++) {
+    let kelimeSayisi = 1;
+    for (let harf of dizi[i]) {
+      if (harf == "") {
+        kelimeSayisi++;
+      }
+    }
+    toplamKelimeSayisi = toplamKelimeSayisi + kelimeSayisi;
+  }
+  let sonuc = toplamKelimeSayisi / dizi.length;
+  return sonuc;
 }
-
+console.log(ortalamaKelimeSayisi(orijinalTatlar));
 
 /* ALIŞTIRMA 2:
 Firma mevcut tatların yanında artık mevsimlik lezzetler ve hatta bölgesel lezzetler de sunmaktadır. Toplam 25 lezzet aromasını
@@ -189,8 +209,7 @@ Aşağıdakileri yapmak için rastgeleTatlar işlevini ve yeni dizileri kullanı
   Örneğin: rastgeleTatlar(orijinalTatlar, yeniTatlar, mevsimlikTatlar, bolgeselTatlar) çalıştırıldığında ["Kestane", "Ballı Badem,"..."Hindistan Cevizi", "Kuru üzüm"].
 */
 
-
-function rastgeleTatlar(/*kod buraya*/){
+function rastgeleTatlar(/*kod buraya*/) {
   /*kod buraya*/
 }
 
@@ -232,11 +251,10 @@ function rastgeleTatlar(/*kod buraya*/){
 // "Karamel"
 // ]
 
-
 /* Lütfen bu satırın altındaki hiçbir şeyi değiştirmeyin */
-function sa(){
-  console.log('Calışıyor');
-  return 'as';
+function sa() {
+  console.log("Calışıyor");
+  return "as";
 }
 sa();
 module.exports = {
@@ -249,6 +267,5 @@ module.exports = {
   kopyala,
   ismeGoreFiltrele,
   ortalamaKelimeSayisi,
-  rastgeleTatlar
-}
-
+  rastgeleTatlar,
+};
--- index.test.js
+++ index.test.js
@@ -1,163 +1,162 @@
-import functions from './index';
+import functions from "./index";
 // import{orijinalTatlar}from '../index';
 let orijinalTatlar;
-beforeEach(()=>{
-    orijinalTatlar = [
-        "Muz",
-        "Vişne",
-        "Ceviz",
-        "Kestane",
-        "Kiraz",
-        "Çikolata",
-        "Fındık Çikolata",
-        "Fıstık Çikolata",
-        "Badem Çikolata",
-        "Franbuaz",
-        "Yaban Mersini",
-        "Tarçın",
-        "Badem",
-        "Kahve",
-        "Fındık",
-        "Anten Fıstık",
-        "Limon",
-        "Misket Limon",
-        "Akçaağaç Şurubu",
-        "Şeftali",
-        "Nane",
-        "Ananas",
-        "Ahududu",
-        "Çilek",
-        "Vanilya",
-      ];
-})
-
-describe('saFunction', ()=>{
-    it('sa -> as ', ()=>{
-        expect(functions.sa()).toBe('as');
-    })
+beforeEach(() => {
+  orijinalTatlar = [
+    "Muz",
+    "Vişne",
+    "Ceviz",
+    "Kestane",
+    "Kiraz",
+    "Çikolata",
+    "Fındık Çikolata",
+    "Fıstık Çikolata",
+    "Badem Çikolata",
+    "Franbuaz",
+    "Yaban Mersini",
+    "Tarçın",
+    "Badem",
+    "Kahve",
+    "Fındık",
+    "Anten Fıstık",
+    "Limon",
+    "Misket Limon",
+    "Akçaağaç Şurubu",
+    "Şeftali",
+    "Nane",
+    "Ananas",
+    "Ahududu",
+    "Çilek",
+    "Vanilya",
+  ];
 });
 
-describe('kopyala', ()=>{
-    it('kopyala -> dizi döner', ()=>{
-        expect(functions.kopyala(orijinalTatlar)).toEqual(orijinalTatlar);
-    })
+describe("saFunction", () => {
+  it("sa -> as ", () => {
+    expect(functions.sa()).toBe("as");
+  });
 });
 
-describe('dizi25Cesitmi', ()=>{
-    it('dizi25Cesitmi -> true döner', ()=>{
-        expect(functions.dizi25Cesitmi(orijinalTatlar)).toBe(true);
-    })
+describe("kopyala", () => {
+  it("kopyala -> dizi döner", () => {
+    expect(functions.kopyala(orijinalTatlar)).toEqual(orijinalTatlar);
+  });
 });
 
-describe('cesitEkle', ()=>{
-    it('cesitEkle -> verilen yeni tadı, verilen dizinin başına ekler', ()=>{
-        expect(functions.cesitEkle(orijinalTatlar, 'Kakule')).toEqual([
-            "Kakule",
-            "Muz",
-            "Vişne",
-            "Ceviz",
-            "Kestane",
-            "Kiraz",
-            "Çikolata",
-            "Fındık Çikolata",
-            "Fıstık Çikolata",
-            "Badem Çikolata",
-            "Franbuaz",
-            "Yaban Mersini",
-            "Tarçın",
-            "Badem",
-            "Kahve",
-            "Fındık",
-            "Anten Fıstık",
-            "Limon",
-            "Misket Limon",
-            "Akçaağaç Şurubu",
-            "Şeftali",
-            "Nane",
-            "Ananas",
-            "Ahududu",
-            "Çilek",
-            "Vanilya",
-          ]);
-    })
+describe("dizi25Cesitmi", () => {
+  it("dizi25Cesitmi -> true döner", () => {
+    expect(functions.dizi25Cesitmi(orijinalTatlar)).toBe(true);
+  });
 });
 
-describe('sonCesitiKaldir', ()=>{
-    it('sonCesitiKaldir -> son öğesi kaldırılmış diziyi döner', ()=>{
-        expect(functions.sonCesitiKaldir(orijinalTatlar)).toEqual([
-            "Muz",
-            "Vişne",
-            "Ceviz",
-            "Kestane",
-            "Kiraz",
-            "Çikolata",
-            "Fındık Çikolata",
-            "Fıstık Çikolata",
-            "Badem Çikolata",
-            "Franbuaz",
-            "Yaban Mersini",
-            "Tarçın",
-            "Badem",
-            "Kahve",
-            "Fındık",
-            "Anten Fıstık",
-            "Limon",
-            "Misket Limon",
-            "Akçaağaç Şurubu",
-            "Şeftali",
-            "Nane",
-            "Ananas",
-            "Ahududu",
-            "Çilek",
-          ]);
-    })
+describe("cesitEkle", () => {
+  it("cesitEkle -> verilen yeni tadı, verilen dizinin başına ekler", () => {
+    expect(functions.cesitEkle(orijinalTatlar, "Kakule")).toEqual([
+      "Kakule",
+      "Muz",
+      "Vişne",
+      "Ceviz",
+      "Kestane",
+      "Kiraz",
+      "Çikolata",
+      "Fındık Çikolata",
+      "Fıstık Çikolata",
+      "Badem Çikolata",
+      "Franbuaz",
+      "Yaban Mersini",
+      "Tarçın",
+      "Badem",
+      "Kahve",
+      "Fındık",
+      "Anten Fıstık",
+      "Limon",
+      "Misket Limon",
+      "Akçaağaç Şurubu",
+      "Şeftali",
+      "Nane",
+      "Ananas",
+      "Ahududu",
+      "Çilek",
+      "Vanilya",
+    ]);
+  });
 });
 
-describe('indekstekiCesitiGetir', ()=>{
-    it('indekstekiCesitiGetir -> indekste bulunan çeşiti döndürür', ()=>{
-        expect(functions.indekstekiCesitiGetir(orijinalTatlar, 2)).toBe("Ceviz");
-    })
+describe("sonCesitiKaldir", () => {
+  it("sonCesitiKaldir -> son öğesi kaldırılmış diziyi döner", () => {
+    expect(functions.sonCesitiKaldir(orijinalTatlar)).toEqual([
+      "Muz",
+      "Vişne",
+      "Ceviz",
+      "Kestane",
+      "Kiraz",
+      "Çikolata",
+      "Fındık Çikolata",
+      "Fıstık Çikolata",
+      "Badem Çikolata",
+      "Franbuaz",
+      "Yaban Mersini",
+      "Tarçın",
+      "Badem",
+      "Kahve",
+      "Fındık",
+      "Anten Fıstık",
+      "Limon",
+      "Misket Limon",
+      "Akçaağaç Şurubu",
+      "Şeftali",
+      "Nane",
+      "Ananas",
+      "Ahududu",
+      "Çilek",
+    ]);
+  });
 });
 
-describe('ismeGoreCesitCikar', ()=>{
-    it('ismeGoreCesitCikar -> diziden istenen öğe kaldırılır', ()=>{
-        expect(functions.ismeGoreCesitCikar(orijinalTatlar, 'Limon')).toEqual([
-            "Muz",
-            "Vişne",
-            "Ceviz",
-            "Kestane",
-            "Kiraz",
-            "Çikolata",
-            "Fındık Çikolata",
-            "Fıstık Çikolata",
-            "Badem Çikolata",
-            "Franbuaz",
-            "Yaban Mersini",
-            "Tarçın",
-            "Badem",
-            "Kahve",
-            "Fındık",
-            "Anten Fıstık",
-            "Misket Limon",
-            "Akçaağaç Şurubu",
-            "Şeftali",
-            "Nane",
-            "Ananas",
-            "Ahududu",
-            "Çilek",
-            "Vanilya",
-          ]);
-    })
+describe("indekstekiCesitiGetir", () => {
+  it("indekstekiCesitiGetir -> indekste bulunan çeşiti döndürür", () => {
+    expect(functions.indekstekiCesitiGetir(orijinalTatlar, 2)).toBe("Ceviz");
+  });
 });
 
-describe('ismeGoreFiltrele', ()=>{
-    it('ismeGoreFiltrele -> istenen tatları içeren yeni diziyi döndürür', ()=>{
-        expect(functions.ismeGoreFiltrele(orijinalTatlar, 'Çikolata')).toEqual([
-            "Çikolata",
-            "Fındık Çikolata",
-            "Fıstık Çikolata",
-            "Badem Çikolata",
-          ]);
-    })
+describe("ismeGoreCesitCikar", () => {
+  it("ismeGoreCesitCikar -> diziden istenen öğe kaldırılır", () => {
+    expect(functions.ismeGoreCesitCikar(orijinalTatlar, "Limon")).toEqual([
+      "Muz",
+      "Vişne",
+      "Ceviz",
+      "Kestane",
+      "Kiraz",
+      "Çikolata",
+      "Fındık Çikolata",
+      "Fıstık Çikolata",
+      "Badem Çikolata",
+      "Franbuaz",
+      "Yaban Mersini",
+      "Tarçın",
+      "Badem",
+      "Kahve",
+      "Fındık",
+      "Anten Fıstık",
+      "Misket Limon",
+      "Akçaağaç Şurubu",
+      "Şeftali",
+      "Nane",
+      "Ananas",
+      "Ahududu",
+      "Çilek",
+      "Vanilya",
+    ]);
+  });
 });
 
+describe("ismeGoreFiltrele", () => {
+  it("ismeGoreFiltrele -> istenen tatları içeren yeni diziyi döndürür", () => {
+    expect(functions.ismeGoreFiltrele(orijinalTatlar, "Çikolata")).toEqual([
+      "Çikolata",
+      "Fındık Çikolata",
+      "Fıstık Çikolata",
+      "Badem Çikolata",
+    ]);
+  });
+});
